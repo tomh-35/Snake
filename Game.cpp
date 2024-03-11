9258f7f8696ca7f977bc51aab75648d9a89feecf
Tommy <Tommyhuang35@outlook.com>
2024-03-10 20:34:27 -0400

    Updated Initialize

--- Game.cpp
+++ Game.cpp
@@ -2,25 +2,22 @@
 #include <iostream>
 
 void Game::initialize(int xb,int yb,int w,int l) {
-	int b = l;
-	int i = l;
-	const int xpos;
-	for (l > 0; --l;) {
-		std::cout<<"+";
-	}
-	std::cout << std::endl;
-	l = i;
-	for(i > 0;--i;){
-		std::cout << "+";
-		for (l >= 2; --l;) {
-			std::cout << " ";
+	int j = 0;
+	int i = 0;
+	//Game::xIndex=w;
+	for(j<l;++j;){
+		for (i < w; ++i;) {
+			if(yb==0||yb==l){
+				std::cout<<"+";
+			}
+			else if(xb==0||xb==w){
+				std::cout<<"+";
+			}
+			else{
+				std::cout<<" ";
+			}		
+			xb++;
 		}
-		std::cout <<"+" << std::endl;
-		l = o;
+	yb++;
 	}
-	l = o;
-	for (l > 0; --l;) {
-		std::cout << "+";
-	}
-	
 }
